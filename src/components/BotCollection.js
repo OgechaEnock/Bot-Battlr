a1420a1bff61d29e88aef437f54ca50c919463c4
OgechaEnock <enockogecha7@gmail.com>
2025-10-19 18:40:19 +0300

    add bootstrap card grid to bot collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -3,13 +3,16 @@ import BotCard from "./BotCard";
 
 function BotCollection({ bots, onAdd }) {
   return (
-    <div className="bot-collection">
-      <h2>Available Bots</h2>
-      <div className="bot-grid">
+    <div className="bg-light p-3 rounded shadow-sm">
+       <h4 className="text-center text-dark mb-3">Available Bots</h4>
+      <div className="row g-3">
         {bots.map((bot) => (
-          <BotCard key={bot.id} bot={bot} onClick={() => onAdd(bot)} />
+          <div key={bot.id} className="col-sm-6 col-md-4 col-lg-3">
+          <BotCard bot={bot} onClick={() => onAdd(bot)} />
+          </div>
         ))}
-      </div>
+
+        </div>
     </div>
   );
 }
