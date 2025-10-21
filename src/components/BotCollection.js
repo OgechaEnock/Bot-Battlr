2f1a3676d419d57098b01d2d49135a9d4da4d0cb
OgechaEnock <enockogecha7@gmail.com>
2025-10-21 16:09:24 +0300

    Add bot collection full functionality

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,18 +1,23 @@
 import React from "react";
 import BotCard from "./BotCard";
+import BotSpecs from "./BotSpecs";
 
-function BotCollection({ bots, onAdd }) {
+function BotCollection({ bots, selectedBot, onSelect, onEnlist, onDelete }) {
+  if (selectedBot) {
+    return (
+      <BotSpecs
+        bot={selectedBot}
+        onBack={() => onSelect(null)}
+        onEnlist={onEnlist}
+        onDelete={onDelete}
+      />
+    );
+  }
   return (
-    <div className="bg-light p-3 rounded shadow-sm">
-       <h4 className="text-center text-dark mb-3">Available Bots</h4>
-      <div className="row g-3">
-        {bots.map((bot) => (
-          <div key={bot.id} className="col-sm-6 col-md-4 col-lg-3">
-          <BotCard bot={bot} onClick={() => onAdd(bot)} />
-          </div>
-        ))}
-
-        </div>
+    <div className="d-flex flex-wrap gap-3 justify-content-center">
+      {bots.map((bot) => (
+        <BotCard key={bot.id} bot={bot} onSelect={onSelect} />
+      ))}
     </div>
   );
 }
